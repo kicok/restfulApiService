fa922e91fd1ed077e06b0477119b361fa5bea6b5
kicok <kicoker@gmail.com>
2020-08-11 00:43:27 +0900

    add flyaway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'daejon', 'firstName 하하하', 'male', 'lastName'),
+	(6, 'sejong in Korean1', 'jane12 2112121', 'feMale', 'kim 121211'),
+	(7, 'sejong in Korean1', 'jane12 V2', 'feMale', 'kim 121211'),
+	(8, 'sejong in Korean1', 'jane12 VoV2', 'feMale', 'kim 121211');
